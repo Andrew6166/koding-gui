09666b56698d4c60e68a67ead196b92dad7d58de
JulienMcJay <julianericain@outlook.com>
2014-11-01 13:56:27 +0100

    Update kodinggui.py

--- kodinggui.py
+++ kodinggui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import getpass
+import sys
 
 def redraw():
     
@@ -35,34 +36,38 @@ def mainmenu():
 x = 0
 
 while x is not "q" or x is not "Q":
-    mainmenu()
-    x = raw_input("Choose an Option. ")
-    
-    if int(x) == 1:
-        print """\
+    try:
+        mainmenu()
+        x = raw_input("Choose an Option. ")
         
-        (1) Start
-        (2) Stop
-        (3) Restart
-        (4) Main menu
+        if int(x) == 1:
+            print """\
+            
+            (1) Start
+            (2) Stop
+            (3) Restart
+            (4) Main menu
+            
+            """
+            
+            a = int(raw_input("Choose an option. "))
+            
+            if a == 1:
+                os.system("sudo service apache2 start")
+            elif a == 2:
+                os.system("sudo service apache2 stop")
+            elif a == 3:
+                os.system("sudo service apache2 restart")
+        elif int(x) == 2:
+            os.system("sudo apt-get install proftpd")
+            os.system("sudo passwd " + str(getpass.getuser()))
+        elif int(x) == 3:
+            os.system("wget http://prdownloads.sourceforge.net/webadmin/webmin_1.710_all.deb")
+            os.system("sudo dpkg -i webmin_1.710_all.deb")
+            os.system("sudo apt-get -f install")
+            os.system("sudo dpkg -i webmin_1.710_all.deb")
+            os.system("rm webmin_1.710_all.deb")
         
-        """
-        
-        a = int(raw_input("Choose an option. "))
-        
-        if a == 1:
-            os.system("sudo service apache2 start")
-        elif a == 2:
-            os.system("sudo service apache2 stop")
-        elif a == 3:
-            os.system("sudo service apache2 restart")
-    elif int(x) == 2:
-        os.system("sudo apt-get install proftpd")
-        os.system("sudo passwd " + str(getpass.getuser()))
-    elif int(x) == 3:
-        os.system("wget http://prdownloads.sourceforge.net/webadmin/webmin_1.710_all.deb")
-        os.system("sudo dpkg -i webmin_1.710_all.deb")
-        os.system("sudo apt-get -f install")
-        os.system("sudo dpkg -i webmin_1.710_all.deb")
-        os.system("rm webmin_1.710_all.deb")
-
+    except (KeyboardInterrupt, SystemExit):
+        print "\n"
+        sys.exit()
