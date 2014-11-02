f6c27ab7592cbff2ab616411f82de7559f3c7187
JulienMcJay <julianericain@outlook.com>
2014-11-02 21:20:59 +0100

    Update kodinggui.py

--- kodinggui.py
+++ kodinggui.py
@@ -26,7 +26,7 @@ redraw()
 def mainmenu():
     print """\
     
-    (1) Apache
+    (1) Services
     (2) Setup FTP
     (3) Setup Webmin
     (^C) Quit
@@ -36,38 +36,83 @@ def mainmenu():
 x = 0
 
 while x is not "q" or x is not "Q":
-    try:
-        mainmenu()
-        x = raw_input("Choose an Option. ")
-        
-        if int(x) == 1:
-            print """\
-            
-            (1) Start
-            (2) Stop
-            (3) Restart
-            (4) Main menu
-            
-            """
-            
-            a = int(raw_input("Choose an option. "))
-            
-            if a == 1:
-                os.system("sudo service apache2 start")
-            elif a == 2:
-                os.system("sudo service apache2 stop")
-            elif a == 3:
-                os.system("sudo service apache2 restart")
-        elif int(x) == 2:
-            os.system("sudo apt-get install proftpd")
-            os.system("sudo passwd " + str(getpass.getuser()))
-        elif int(x) == 3:
-            os.system("wget http://prdownloads.sourceforge.net/webadmin/webmin_1.710_all.deb")
-            os.system("sudo dpkg -i webmin_1.710_all.deb")
-            os.system("sudo apt-get -f install")
-            os.system("sudo dpkg -i webmin_1.710_all.deb")
-            os.system("rm webmin_1.710_all.deb")
-        
-    except (KeyboardInterrupt, SystemExit):
-        print "\n"
-        sys.exit()
+	try:
+		mainmenu()
+		x = raw_input("Choose an Option. ")
+		
+		if int(x) == 1:
+			redraw()
+			print """\
+			
+			(1) Apache
+			(2) FTP
+			(3) Webmin
+			
+			"""
+			b = raw_input("Choose an Option. ")
+			if int(b) == 1:
+				print """\
+				
+				(1) Start
+				(2) Stop
+				(3) Restart
+				(4) Main menu
+				
+				"""
+				
+				a = int(raw_input("Choose an option. "))
+				
+				if a == 1:
+					os.system("sudo service apache2 start")
+				elif a == 2:
+					os.system("sudo service apache2 stop")
+				elif a == 3:
+					os.system("sudo service apache2 restart")
+			elif int(b) == 2:
+				print """\
+				
+				(1) Start
+				(2) Stop
+				(3) Restart
+				(4) Main menu
+				
+				"""
+				
+				a = int(raw_input("Choose an option. "))
+				
+				if a == 1:
+					os.system("sudo service proftpd start")
+				elif a == 2:
+					os.system("sudo service proftpd stop")
+				elif a == 3:
+					os.system("sudo service proftpd restart")
+			elif int(b) == 3:
+				print """\
+				
+				(1) Start
+				(2) Stop
+				(3) Restart
+				(4) Main menu
+				
+				"""
+				
+				a = int(raw_input("Choose an option. "))
+				
+				if a == 1:
+					os.system("sudo service webmin start")
+				elif a == 2:
+					os.system("sudo service webmin stop")
+				elif a == 3:
+					os.system("sudo service webmin restart")
+		elif int(x) == 2:
+			os.system("sudo apt-get install proftpd")
+			os.system("sudo passwd " + str(getpass.getuser()))
+		elif int(x) == 3:
+			os.system("wget http://prdownloads.sourceforge.net/webadmin/webmin_1.710_all.deb")
+			os.system("sudo dpkg -i webmin_1.710_all.deb")
+			os.system("sudo apt-get -f install")
+			os.system("sudo dpkg -i webmin_1.710_all.deb")
+			os.system("rm webmin_1.710_all.deb")
+	except KeyboardInterrupt:
+		print
+		sys.exit()
